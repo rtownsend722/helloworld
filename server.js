a7b219adca0a7126e17e46bb23f8da13a1e44823
Rebecca Townsend <rebecca.townsend722@gmail.com>
2018-03-05 15:10:22 -0800

    create public dir

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 let port = process.env.PORT || 8080;
+
+app.use(express.static('public'));
 
 app.listen(port, '127.0.0.1', () => {
   console.log(`Hello World is listening on port ${port}`);
