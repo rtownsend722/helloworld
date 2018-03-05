876e1a642681c635102cc396c6f3f4ac495b4424
Rebecca Townsend <rebecca.townsend722@gmail.com>
2018-03-05 15:02:32 -0800

    update host

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('express')();
 let port = process.env.PORT || 8080;
 
-app.listen(port, () => {
+app.listen(port, '127.0.0.1', () => {
   console.log(`Hello World is listening on port ${port}`);
 })
